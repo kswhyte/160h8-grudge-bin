757e92810362270eeae19f7947e47b92d864dbed
KINAN WHYTE <kinanwhyte@gmail.com>
2017-02-23 12:17:22 -0700

    [ADD]: Create html form template for adding grudges; Add nodemon, jquery, es6-promises, shortid dependencies

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const shortid = require('shortid');
+// console.log(shortid.generate());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -8,13 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 1111);
 app.locals.title = '160h8 Grudge Bin';
 
-app.locals.name = {
-  firstName: 'Kinan'
-};
+app.locals.grudges = {}
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
+app.get('/', (req, res) => {
+  res.redirect('/grudges')
+})
+
+app.get('/grudges', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
